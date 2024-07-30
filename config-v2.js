4df16ec8f15ffb0c7296e86f8caa0a89c008c146
Vimukthiguna <vimukthigunawardhana356@gmail.com>
2024-07-31 00:02:36 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
 const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const SESSION_ID = "PRABATH-MD~AvMzUR6L#57wW3zD8s628OVGTO1asYnPACbXiJx7jihXYg3tJHm8"
 
 
 module.exports = {
